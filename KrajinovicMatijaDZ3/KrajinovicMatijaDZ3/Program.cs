41638afe92d9653876645680ecbefab8f3bf96d1
Maitja <44928436+Maitja@users.noreply.github.com>
2018-12-15 14:16:31 +0100

    Add files via upload

---
+++ KrajinovicMatijaDZ3/KrajinovicMatijaDZ3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrajinovicMatijaDZ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite putanju izvornog direktorija:");
+            string izvorniDirektorij = Console.ReadLine();
+            Console.Write("Unesite putanju ciljnog direktorija:");
+            string ciljniDirektorij = Console.ReadLine();
+            try
+            {
+                
+            foreach (string datoteka
+            in Directory.GetFiles(izvorniDirektorij))
+                {
+                    
+                    string imeDatoteke = Path.GetFileName(datoteka);
+                    
+                    string ciljnaDatoteka
+                    = Path.Combine(ciljniDirektorij,
+
+                    imeDatoteke);
+
+                   
+                    File.Copy(datoteka, ciljnaDatoteka, true);
+                }
+                Console.WriteLine("Gotovo!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Greška: { 0}", ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ KrajinovicMatijaDZ4/KrajinovicMatijaDZ4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrajinovicMatijaDZ4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite putanju direktorija: ");
+            string putanja = Console.ReadLine();
+            
+            DirectoryInfo diIzvor = new DirectoryInfo(putanja);
+            
+            Console.WriteLine("\n-- Poddirektoriji:");
+            foreach (DirectoryInfo di in diIzvor.GetDirectories())
+            {
+             Console.WriteLine("{0}\t{1}\t{2}",di.Name, di.CreationTime, di.LastAccessTime);
+            }
+            
+            Console.WriteLine("\n-- Datoteke:");
+            foreach (FileInfo fi in diIzvor.GetFiles())
+            {
+                Console.WriteLine("{0}\t{1}\t{2}",fi.Name, fi.CreationTime, fi.LastAccessTime);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
