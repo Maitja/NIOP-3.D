b2eb0568dc3ad85c107172c11ac9c46e4a360d63
Maitja <44928436+Maitja@users.noreply.github.com>
2019-01-16 17:34:16 +0100

    Add files via upload

---
+++ StramReaderKrajinovicMatija/StramReaderKrajinovicMatija/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StramReaderKrajinovicMatija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\Učenik\source\repos\StramReaderKrajinovicMatija\haha.txt");
+            
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            
+            sr.Close();
+            Console.ReadKey();
+        }
+    }
+}
