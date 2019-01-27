52ed88176a69111cb7a9087096c1a7cf0851034d
Maitja <44928436+Maitja@users.noreply.github.com>
2019-01-27 12:37:03 +0100

    Add files via upload

---
+++ MonthcalendarMatijaKrajinovic/MonthcalendarMatijaKrajinovic/Form3.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MonthcalendarMatijaKrajinovic
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(331, 256);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 71);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Month calendar,                    Pokazuje odabrani termin.       Matija Krajino" +
+    "vic 3.D 27.1.2019.";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(240, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(320, 57);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "\"Davatelj licence dopušta umnožavanje, distribuiranje i priopćavanje djela javnos" +
+    "ti. Zauzvrat primatelji licence moraju imenovanjem priznati i označiti izvornog " +
+    "autora.\"";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(334, 124);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
