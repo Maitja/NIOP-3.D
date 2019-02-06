0279534bdc8797e784ae5a8c98f2da587ee0fa98
Maitja <44928436+Maitja@users.noreply.github.com>
2019-02-06 12:10:19 +0100

    Add files via upload

---
+++ KrajinovicMatijaKlaseitd/KrajinovicMatijaKlaseitd/Klase/Vozilo.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KrajinovicMatijaKlaseitd.Klase
+{
+    class Vozilo
+    {
+        int brojKotaca;
+        bool leti, pliva, vozi;
+        string vrsta;
+        string oznaka;
+
+        public void setBrojKotaca(int brojKotaca)
+        { this.brojKotaca = brojKotaca;
+        }
+
+        public int getBrojKotaca()
+        { return this.brojKotaca; }
+
+        public void setLeti(bool leti)
+        {
+            this.leti = leti;
+        }
+
+        public bool getleti()
+        { return this.leti; }
+
+        public void setPliva(bool pliva)
+        {
+            this.pliva = pliva;
+        }
+
+        public bool getPliva()
+        { return this.pliva; }
+
+        public void setVozi(bool vozi)
+        {
+            this.vozi = vozi;
+        }
+
+        public bool getVozi()
+        { return this.vozi; }
+
+        public void setVrsta(string vrsta)
+        { this.vrsta = vrsta; }
+
+        public string getVrsta()
+        { return this.vrsta; }
+
+        public void setOznaka(string oznaka)
+        {
+            this.oznaka = oznaka;
+        }
+
+        public string getOznaka()
+        { return this.oznaka; }
+
+        public Vozilo(int brojKotaca, bool leti, bool pliva, bool vozi, string vrsta, string oznaka)
+        {
+            this.brojKotaca = brojKotaca;
+            this.leti = leti;
+            this.pliva = pliva;
+            this.vozi = vozi;
+            this.vrsta = vrsta;
+            this.oznaka = oznaka;
+
+        }
+
+        public Vozilo()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return " Vrsta : " + vrsta + "\n Oznaka : " + oznaka + "\n Pliva : " + 
+                pliva + "\n Leti : " + leti + "\n Vozi : " + vozi + "\n Broj Kotaca : " +
+                brojKotaca;
+        }
+
+
+
+    }
+
+}
---
+++ KrajinovicMatijaKlaseitd/KrajinovicMatijaKlaseitd/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using KrajinovicMatijaKlaseitd.Klase;
+
+namespace KrajinovicMatijaKlaseitd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vozilo avijon = new Vozilo();
+
+            avijon.setBrojKotaca(6);
+            avijon.setLeti(true);
+            avijon.setPliva(false);
+            avijon.setVozi(true);
+            avijon.setVrsta("Cessna");
+            avijon.setOznaka("1312");
+
+            
+
+            Vozilo kamijon = new Vozilo(8,false,false,true,"TAM","RI1987AR");
+
+            Console.WriteLine("Vrsta : " + avijon.getVrsta() + "Oznaka :" + avijon.getOznaka() + "\nLeti : " + avijon.getleti() +
+                "\nPliva : " + avijon.getPliva() + "\nVozi : " + avijon.getVozi() + "\nBroj kotaca : " + avijon.getBrojKotaca())
+                ;
+
+            Console.WriteLine(kamijon.ToString());
+
+
+            Console.ReadLine();
+
+        }
+    }
+
+}
